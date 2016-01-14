1d6994dd76f7727c4ed9b33b759f9998de3349ea
Jesse Friedman <jesse@jesse.ws>
2016-01-14 05:31:59 -0500

    Changing console.log upon application start

--- index.js
+++ index.js
@@ -112,5 +112,5 @@ var server = app.listen(port, function() {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('App listening at http://%s:%s', host, port);
 });
